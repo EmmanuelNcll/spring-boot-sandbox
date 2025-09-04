5250f20c93798c4b4caf07e1e4286ad765660494
EmmanuelNcll <218631373+EmmanuelNcll@users.noreply.github.com>
2025-09-04 12:43:19 +0200

    Enhance certificate management and other minor fixes (#6)
    * Enhance keystore management
    
    * Enhance database container management

--- run.sh
+++ run.sh
@@ -2,9 +2,20 @@
 
 cd "$(dirname "$0")"
 
+is_container_running() {
+  docker ps -q --filter "name=spring-api-db-postgres" --filter "status=running" >/dev/null
+  return $?
+}
+
 echo "Starting PostgreSQL Docker container..."
-if ! docker ps -q --filter "name=spring-api-db-postgres" | grep -q .; then
+if ! is_container_running; then
   docker run --name spring-api-db-postgres -e POSTGRES_PASSWORD=Password#1 -d -p 5432:5432 postgres
+  sleep 5
+  
+  if ! is_container_running; then
+    echo "Error: Failed to start PostgreSQL Docker container."
+    exit 1
+  fi
 fi
 
 echo Building and starting the Spring Boot application...
